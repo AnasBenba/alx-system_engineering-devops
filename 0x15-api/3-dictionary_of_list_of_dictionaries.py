ec9f80938dd6eaea260b547dc69828d477663cd9
AnasBenba <anasbenba123@gmail.com>
2024-02-01 17:23:31 +0100

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,23 +4,30 @@
 import json
 import requests
 
-if __name__ == '__main__':
-    users_url = 'https://jsonplaceholder.typicode.com/users/'
-    response = requests.get(users_url)
-    users = response.json()
-    dictionary = {}
-    for user in users:
-        user_id = user['id']
-        username = user['username']
-        u_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos/'
-        response = requests.get(u_url)
-        tasks = response.json()
-        dictionary[user_id] = []
-        for task in tasks:
-            dictionary[user_id].append({
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": username
-            })
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(dictionary, file)
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo = todos.json()
+    data = {}
+    id = str(todo[0]['userId'])
+    data[id] = []
+    my_user = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{id}').json()
+    for t in todo:
+        record = {
+            "username": my_user['username'],
+            "task": t['title'],
+            "completed": t['completed']}
+        if str(t['userId']) == id:
+            data[id].append(record)
+        else:
+            id = str(t['userId'])
+            data[id] = []
+            my_user = requests.get(
+                f'https://jsonplaceholder.typicode.com/users/{id}').json()
+            record = {
+                "username": my_user['username'],
+                "task": t['title'],
+                "completed": t['completed']}
+            data[id].append(record)
+    with open(f'todo_all_employees.json', 'w') as json_file:
+        json.dump(data, json_file)

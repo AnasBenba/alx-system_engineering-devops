e8136c6e8b3afedc054cfd0769e4509dd845d91a
AnasBenba <anasbenba123@gmail.com>
2024-02-01 17:12:23 +0100

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,20 +5,19 @@ import json
 import requests
 import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    response = requests.get(url)
-    username = response.json().get('username')
-    url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-    response = requests.get(url)
-    tasks = response.json()
-    dictionary = {user_id: []}
-    for task in tasks:
-        dictionary[user_id].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-    with open(f'{user_id}.json', 'w') as file:
-        json.dump(dictionary, file)
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    todos = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{id}/todos')
+    my_user = user.json()
+    todo = todos.json()
+    data = {f"{id}": []}
+    for t in todo:
+        record = {
+            "task": t['title'],
+            "completed": t['completed'],
+            "username": my_user['username']}
+        data[id].append(record)
+        with open(f'{id}.json', 'w') as json_file:
+            json.dump(data, json_file)
